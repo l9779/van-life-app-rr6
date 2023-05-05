798f16180bd8aae25559da4b8b91c02a833b119c
Lucas <lucas.b.dev@proton.me>
2023-05-05 12:49:30 -0300

    braeak

--- src/Pages/Host/HostLayout.jsx
+++ src/Pages/Host/HostLayout.jsx
@@ -3,21 +3,21 @@ import { Link, Outlet } from 'react-router-dom';
 const HostLayout = () => {
   return (
     <main className='min-h-screen p-6 bg-orange-50'>
-      <nav className='space-x-6'>
+      <nav className='flex gap-1'>
         <Link
-          className='w-12 text-zinc-800 hover:underline hover:text-black hover:font-semibold'
+          className='text-center w-24 text-zinc-800 hover:underline hover:text-black hover:font-semibold'
           to='/host'
         >
           Dashboard
         </Link>
         <Link
-          className='w-12 text-zinc-800 hover:underline hover:text-black hover:font-semibold'
+          className='text-center w-24 text-zinc-800 hover:underline hover:text-black hover:font-semibold'
           to='/host/income'
         >
           Income
         </Link>
         <Link
-          className='w-12 text-zinc-800 hover:underline hover:text-black hover:font-semibold'
+          className='text-center w-24 text-zinc-800 hover:underline hover:text-black hover:font-semibold'
           to='/host/reviews'
         >
           Reviews
