96c5a4943095202adaa79b64c5909bfa44931d88
Lucas <lucas.b.dev@proton.me>
2023-05-15 12:24:24 -0300

    b

--- src/server.js
+++ src/server.js
@@ -85,7 +85,7 @@ createServer({
     this.namespace = 'api';
     this.logging = false;
     this.timing = 1000;
-    this.passthrough('https://firesotre.googleapis.com/**');
+    this.passthrough('https://firestore.googleapis.com/**');
 
     this.get('/vans', (schema) => {
       // return new Response(400, {}, {error: "Error fetching data"})
